ebe6bbacf0f6975541f48cbccc69b311912566a0
CodeOnSunday <codeonsunday@gmail.com>
2024-06-03 15:47:12 +0700

    add wiredash analytics

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,16 @@ import 'package:buildr_studio/repositories/tool_repository.dart';
 import 'package:buildr_studio/screens/splash_screen.dart';
 import 'package:context_menus/context_menus.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_it/get_it.dart';
 import 'package:re_highlight/languages/all.dart';
 import 'package:re_highlight/re_highlight.dart';
+import 'package:wiredash/wiredash.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await setupDependencyInjection();
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
@@ -20,27 +23,21 @@ Future<void> setupDependencyInjection() async {
   GetIt.I.registerSingleton(highlight);
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
     return ContextMenuOverlay(
-      child: MaterialApp(
-        title: 'Tool App',
-        theme: AppTheme.blackAndWhiteTheme,
-        debugShowCheckedModeBanner: false,
-        home: const SplashScreen(),
+      child: Wiredash(
+        projectId: dotenv.env['WIREDASH_PROJECT_ID'] ?? '',
+        secret: dotenv.env['WIREDASH_SECRET'] ?? '',
+        child: MaterialApp(
+          title: 'Tool App',
+          theme: AppTheme.blackAndWhiteTheme,
+          debugShowCheckedModeBanner: false,
+          home: const SplashScreen(),
+        ),
       ),
     );
   }
